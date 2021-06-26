afc554005f9f9cb2439c4e477adaf0e343c665bd
Gregor Dschung <gregor@chkpnt.de>
2021-06-26 21:02:49 +0200

    Auf geht’s, ab geht’s, 3 Tage wach

---
+++ src/main/java/de/chkpnt/vaadin/issue1849/views/about/AboutView.java
@@ -0,0 +1,18 @@
+package de.chkpnt.vaadin.issue1849.views.about;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import de.chkpnt.vaadin.issue1849.views.MainLayout;
+
+@Route(value = "about", layout = MainLayout.class)
+@PageTitle("About")
+public class AboutView extends Div {
+
+    public AboutView() {
+        addClassName("about-view");
+        add(new Text("Content placeholder"));
+    }
+
+}
